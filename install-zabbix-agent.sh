24987798d0482d061bf6a0e99ae753b0bd346064
buldozzzer <48224149+buldozzzer@users.noreply.github.com>
2025-11-05 13:01:59 +0300

    Create install-zabbix-agent.sh

---
+++ install-zabbix-agent.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+. /etc/os-release && \
+wget https://repo.zabbix.com/zabbix/7.4/release/debian/pool/main/z/zabbix-release/zabbix-release_latest_7.4+${ID}${VERSION_ID}_all.deb
+# echo https://repo.zabbix.com/zabbix/7.4/release/debian/pool/main/z/zabbix-release/zabbix-release_latest_7.4+${ID}${VERSION_ID}_all.deb
