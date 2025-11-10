d881f171d3358ee6b9c816a73f5a9f5d5dfa2309
buldozzzer <48224149+buldozzzer@users.noreply.github.com>
2025-11-10 15:34:06 +0300

    Update total_traffic.py

--- total_traffic.py
+++ total_traffic.py
@@ -5,7 +5,7 @@ import subprocess
 import json
 import os
 
-INTERFACE = "ens5"
+INTERFACE = "ens5" # имя интерфейса
 LIMIT = 1999 # лимит в GB
 
 logging.basicConfig(filename='/var/log/traffic.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
